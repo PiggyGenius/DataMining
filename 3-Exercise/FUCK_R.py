dd0b11d348e37ed6b5a9501dffb3f568187b9c20
piggygenius <ludo.itrts@gmail.com>
2017-04-29 11:18:44 +0200

    Split is done

--- 3-Exercise/FUCK_R.py
+++ 3-Exercise/FUCK_R.py
@@ -1,13 +1,12 @@
 #! /usr/bin/env python3
 #-*- coding:utf-8 -*-
-
 import scipy.sparse as sp
 import pandas as pd
-
+from sklearn.model_selection import train_test_split
 
 classes = []
 rows = 0
-vocab_size = 141144
+vocab_size = 141145
 
 with open("BaseReuters-29", "r") as f:
     content = f.readlines()
@@ -17,7 +16,6 @@ with open("BaseReuters-29", "r") as f:
 
     i = 0
     for line in content:
-        print(i)
         words = line.split(' ')
         words.pop() # remove '\n' at the end
         classes.append(int(words.pop(0)))
@@ -26,6 +24,5 @@ with open("BaseReuters-29", "r") as f:
             docs[i, int(val[0])] = int(val[1])
         i += 1
 
-
-#  print(classes)
-
+# We randomly split the dataset using sklearn.train_test_split
+train_values, test_values, train_classes, test_classes = train_test_split(docs, classes, train_size = 52500, test_size = 18203, random_state = 1)
