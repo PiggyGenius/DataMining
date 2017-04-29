3310788a462392c249f7ccd44f150e0c9c602b7f
piggygenius <ludo.itrts@gmail.com>
2017-04-29 12:22:25 +0200

    Multinomial theta is calculated

--- 3-Exercise/FUCK_R.py
+++ 3-Exercise/FUCK_R.py
@@ -8,9 +8,11 @@ from sklearn.model_selection import train_test_split
 
 def load_classes():
     classes = []
+    class_count = 29
     rows = 0
     vocab_size = 141145
     i = 0
+    class_term_frequency = [[0 for i in range(vocab_size)] for j in range(29)]
     #  max_size = 0
 
     with open("BaseReuters-29", "r") as f:
@@ -21,20 +23,22 @@ def load_classes():
         for line in content:
             words = line.split(' ')
             words.pop() # remove '\n' at the end
-            classes.append(int(words.pop(0)))
+            class_index = (int(words.pop(0)))
+            classes.append(class_index)
             for word in words:
                 val = word.split(':')
                 #  if (int(val[0]) > max_size):
                 #      max_size = int(val[0])
                 docs[i, int(val[0])] = int(val[1])
+                class_term_frequency[class_index-1][int(val[0])] += int(val[1])
             i += 1
-    return (rows, vocab_size, classes, docs)
+    return (rows, vocab_size, classes, docs, class_term_frequency)
 
 if __name__=="__main__":
     # Do it only once, the result is stored in classes.npy
     # Then, load it (faster than parsing again)
     np.save("classes.npy", load_classes())
-    rows, vocab_size, classes, docs = np.load("classes.npy")
+    rows, vocab_size, classes, docs, class_term_frequency = np.load("classes.npy")
 
 
     print("Size :", (rows, vocab_size))
