635cc282fcefb3d258320e0eb8bed7f5245b4ebb
piggygenius <ludo.itrts@gmail.com>
2017-04-30 09:55:51 +0200

    not done

--- 3-Exercise/FUCK_R.py
+++ 3-Exercise/FUCK_R.py
@@ -1,8 +1,11 @@
 #! /usr/bin/env python3
 #-*- coding:utf-8 -*-
+import itertools
+import random
 import scipy.sparse as sp
 import pandas as pd
 import numpy as np
+import math
 from sklearn.model_selection import train_test_split
 
 
@@ -12,7 +15,8 @@ def load_classes():
     rows = 0
     vocab_size = 141145
     i = 0
-    class_term_frequency = [[0 for i in range(vocab_size)] for j in range(29)]
+    class_term_frequency = [[[1, 0] for i in range(vocab_size)] for j in range(class_count)]
+    class_doc_frequency = [2 for i in range(class_count)]
     #  max_size = 0
 
     with open("BaseReuters-29", "r") as f:
@@ -30,21 +34,32 @@ def load_classes():
                 #  if (int(val[0]) > max_size):
                 #      max_size = int(val[0])
                 docs[i, int(val[0])] = int(val[1])
-                class_term_frequency[class_index-1][int(val[0])] += int(val[1])
+                class_term_frequency[class_index-1][int(val[0])][0] += 1
+                class_term_frequency[class_index-1][int(val[0])][1] += int(val[1])
             i += 1
-    return (rows, vocab_size, classes, docs, class_term_frequency)
-
-if __name__=="__main__":
-    # Do it only once, the result is stored in classes.npy
-    # Then, load it (faster than parsing again)
-    np.save("classes.npy", load_classes())
-    rows, vocab_size, classes, docs, class_term_frequency = np.load("classes.npy")
-
-
     print("Size :", (rows, vocab_size))
     print("Number of documents per class :")
     for elt in [[x, classes.count(x)] for x in set(classes)]:
         print("Class", elt[0], ":", elt[1], "documents")
+        class_doc_frequency[elt[0]-1] += elt[1]
+    return (rows, vocab_size, classes, docs, class_term_frequency, class_doc_frequency)
+
+if __name__=="__main__":
+    # Do it only once, the result is stored in classes.npy
+    # Then, load it (faster than parsing again)
+    # np.save("classes.npy", load_classes())
+    rows, vocab_size, classes, docs, class_term_frequency, class_doc_frequency = np.load("classes.npy")
+
 
     # We randomly split the dataset using sklearn.train_test_split
-    train_values, test_values, train_classes, test_classes = train_test_split(docs, classes, train_size = 52500, test_size = 18203, random_state = 1)
+    train_size = 52500
+    test_size = 18203
+    train_values, test_values, train_classes, test_classes = train_test_split(docs, classes, train_size = train_size, test_size = test_size, random_state = 1)
+
+    # We predict the testing set
+    cx = test_values.tocoo()
+    for i,j,v in zip(cx.row, cx.col, cx.data):
+        print(i,j,v)
+        max_value = 0
+        for k in range(len(classes)):
+            max_value = math.log1p(class_doc_frequency / train_size)
