cf167a01a7003d7adad4f414dcbc34ee9739dbf3
Maxime Deloche <maxime.deloche@ensimag.grenoble-inp.fr>
2017-05-03 19:06:02 +0200

    Added training of multinomial model

--- 3-Exercise/main.py
+++ 3-Exercise/main.py
@@ -22,16 +22,49 @@ def read_data():
             dico = {}
             words = line.split(' ')
             words.pop() # remove '\n' at the end
-            classes.append(int(words.pop(0)))
+            classes.append(int(words.pop(0))-1)
             for word in words:
                 val = word.split(':')
-                dico[int(val[0])] = int(val[1])
+                dico[int(val[0])-1] = int(val[1])
             docs.append(dico)
     return classes, docs
 
 
+def count_documents(train_classes):
+    return [train_classes.count(x) for x in range(class_count)]
+
+
+def train_multinomial(values, classes):
+    m = len(values)
+    counts = count_documents(classes)
+
+    D = np.zeros(class_count)
+    Pi = np.zeros(class_count)
+    tf = np.zeros((class_count, vocab_size))
+    PC = np.zeros((class_count, vocab_size))
+
+    for k in range(class_count):
+        Pi[k] = counts[k]/m
+
+    for k, doc in zip(classes, values):
+        for i in doc:
+            tf[k][i] += doc[i]
+            D[k] += tf[k][i]
+
+    for k in range(class_count):
+        for i in range(vocab_size):
+            PC[k][i] = (tf[k][i] + 1)/(D[k] + vocab_size)
+
+    return Pi, PC
+
+
+def test_multinomial():
+    return
+
+
+
 if __name__=="__main__":
-    print("Readind data")
+    print("Reading data")
     classes, docs = read_data()
 
     # We randomly split the dataset using sklearn.train_test_split
@@ -40,23 +73,30 @@ if __name__=="__main__":
     test_size = 18203
     train_values, test_values, train_classes, test_classes = train_test_split(docs, classes, train_size = train_size, test_size = test_size, random_state = 1)
 
-    class_term_frequency = [[[0, 0] for i in range(vocab_size)] for j in range(class_count)]
-    class_doc_frequency = [0 for i in range(class_count)]
+    #  class_term_frequency = [[[0, 0] for i in range(vocab_size)] for j in range(class_count)]
+    #  class_doc_frequency = [0 for i in range(class_count)]
 
     # We store the number of documents in each class containing each term in [0]
     # We store the sum of term frequency for all documents in each class in [1]
-    cx = train_values.tocoo()
-    for i, j, v in zip(cx.row, cx.col, cx.data):
-        class_term_frequency[classes[i] - 1][j][0] += 1
-        class_term_frequency[classes[i] - 1][j][1] += v
+    #  cx = train_values.tocoo()
+    #  for i, j, v in zip(cx.row, cx.col, cx.data):
+    #      class_term_frequency[classes[i] - 1][j][0] += 1
+    #      class_term_frequency[classes[i] - 1][j][1] += v
 
     print("Size :", (rows, vocab_size))
     print("Number of documents per class :")
-    for elt in [[x, classes.count(x)] for x in range(1, class_count+1)]:
-        print("Class", elt[0], ":", elt[1], "documents")
+    
+    i = 1
+    for elt in count_documents(classes):
+        print("Class", i, ":", elt, "documents")
+        i += 1
 
     # computation of the class_doc_frequency (on the training set)
-    class_doc_frequency = [train_classes.count(x) for x in range(1, class_count+1)]
+    #  class_doc_frequency = [train_classes.count(x) for x in range(1, class_count+1)]
+
+    print("Training multinomial model")
+    Pi, PC = train_multinomial(train_values, train_classes)
+    exit()
 
 
 
